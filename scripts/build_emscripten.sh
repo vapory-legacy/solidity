a304fcdb593eae12e336447ebf6848ba6a4f35b7
Paweł Bylica <chfast@gmail.com>
2017-08-29 00:09:47 +0200

    Upgrade Emscripten SDK

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -30,5 +30,5 @@ set -e
 
 if [[ "$OSTYPE" != "darwin"* ]]; then
     ./scripts/travis-emscripten/install_deps.sh
-    docker run -v $(pwd):/src trzeci/emscripten:sdk-tag-1.35.4-64bit ./scripts/travis-emscripten/build_emscripten.sh
+    docker run -v $(pwd):/src trzeci/emscripten:sdk-tag-1.37.19-64bit ./scripts/travis-emscripten/build_emscripten.sh
 fi
